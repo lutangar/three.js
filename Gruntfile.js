05798afcc03d46d9ce7b0c3aa1c6f1dde7e5ea27
lutangar <johan.dufour@gmail.com>
2014-01-06 11:29:33 +0100

    fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,21 @@
-grunt.initConfig({
-    execute: {
-        build: {
-            src: ['./utils/build/build.js']
+module.exports = function(grunt) {
+    grunt.initConfig({
+        execute: {
+            build: {
+                src: ['./utils/build/build.js']
+            }
+        },
+
+        browserify2: {
+            compile: {
+                entry: './build/three.js',
+                compile: './three.js'
+            }
         }
-    },
+    });
 
-    browserify2: {
-        compile: {
-            entry: './build/three.js',
-            compile: './three.js'
-        }
-    }
-});
+    grunt.loadNpmTasks('grunt-execute');
+    grunt.loadNpmTasks('grunt-browserify2');
 
-grunt.loadNpmTasks('grunt-execute');
-grunt.loadNpmTasks('grunt-browserify2');
-
-grunt.registerTask('default', ['execute:build', 'browserify2:compile']);
+    grunt.registerTask('default', ['execute:build', 'browserify2:compile']);
+};
