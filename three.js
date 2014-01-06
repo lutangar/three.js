44d47d12ea0d47792f44682c22e18a0aadb0cfce
lutangar <johan.dufour@gmail.com>
2014-01-06 14:14:48 +0100

    add browserified build to module

--- three.js
+++ three.js
@@ -1,4 +1,5 @@
-/**
+;(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
+(function(){/**
  * @author mrdoob / http://mrdoob.com/
  * @author Larry Battle / http://bateru.com/news
  * @author bhouston / http://exocortex.com
@@ -37192,3 +37193,7 @@ THREE.ShaderFlares = {
 	}
 
 };
+
+})()
+},{}]},{},[1])
+;
